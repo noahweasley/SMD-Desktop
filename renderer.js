48b62bd0ecf247812d7336947436575bef9d80da
Noah <iebenmelu@gmail.com>
2021-11-16 20:17:22 -0800

    refactored code

--- renderer.js
+++ renderer.js
@@ -1,10 +1,11 @@
 window.addEventListener('DOMContentLoaded', () => {
-
+    // window action button clicked
     document.querySelectorAll('.window-action').forEach((action) => {
-
         action.addEventListener('click', event => {
             window.bridgeApis.send('action-click-event', action.id)
         })
 
     })
+    
+    //...
 })
