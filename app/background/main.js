4f8703850665ddadd5df3b129d776ac1a31cf740
Noah <iebenmelu@gmail.com>
2021-12-16 22:14:30 -0800

    updated project, still in development

--- app/background/main.js
+++ app/background/main.js
@@ -11,7 +11,83 @@ const {
 
 const path = require('path')
 
-const SpotifyWebApi = require('spotify-web-api-node')
+const {
+    SpotifyURLType,
+    getSpotifyURLType
+} = require('../background/util')
+
+const express = require('../../node_modules/express');
+const SpotifyWebApi = require('spotify-web-api-node');
+
+// credentials are optional
+const spotifyApi = new SpotifyWebApi({
+
+});
+
+const scopes = [
+    'ugc-image-upload',
+    'user-read-playback-state',
+    'user-modify-playback-state',
+    'user-read-currently-playing',
+    'streaming',
+    'app-remote-control',
+    'user-read-email',
+    'user-read-private',
+    'playlist-read-collaborative',
+    'playlist-modify-public',
+    'playlist-read-private',
+    'playlist-modify-private',
+    'user-library-modify',
+    'user-library-read',
+    'user-top-read',
+    'user-read-playback-position',
+    'user-read-recently-played',
+    'user-follow-read',
+    'user-follow-modify'
+];
+
+const server = express();
+
+server.get('/login', (req, res) => {
+    res.redirect(spotifyApi.createAuthorizeURL(scopes));
+});
+
+server.get('/callback', (req, res) => {
+    const error = req.query.error;
+    const code = req.query.code;
+    const state = req.query.state;
+
+    if (error) {
+        console.error('Callback Error:', error);
+        res.send(`Callback Error: ${error}`);
+        return;
+    }
+
+    spotifyApi
+        .authorizationCodeGrant(code)
+        .then(data => {
+            const access_token = data.body['access_token'];
+            const refresh_token = data.body['refresh_token'];
+            const expires_in = data.body['expires_in'];
+
+            spotifyApi.setAccessToken(access_token);
+            spotifyApi.setRefreshToken(refresh_token);
+
+            res.send('Success! You can now close the window.');
+
+            setInterval(async () => {
+                const data = await spotifyApi.refreshAccessToken();
+                const access_token = data.body['access_token'];
+                spotifyApi.setAccessToken(access_token);
+            }, expires_in / 2 * 1000);
+        })
+});
+
+server.listen(8888, () =>
+    shell.openExternal("http://localhost:8888/login")
+);
+
+// ---------------------------------------------------------------------------------
 
 let smd_window
 let WINDOW_STATE
@@ -63,7 +139,28 @@ ipcMain.handle('clipboard-request', () => {
     const spotifyLinkRegex = new RegExp('https://open.spotify.com')
     if (spotifyLinkRegex.test(clipboardContent)) {
         // then ...
+        let spotifyURLType = getSpotifyURLType(clipboardContent)
+        switch (spotifyURLType) {
+            case SpotifyURLType.TRACK:
+                let track = clipboardContent.substring("https://open.spotify.com/track/".length, clipboardContent.length)
 
+                spotifyApi.getTrack(track)
+                    .then(data => {
+                        const body = data.body
+                        let name = body['name']
+                        let artists = body.artists
+                        let artistNames = []
+
+                        artists.forEach(artist => {
+                            artistNames.push(artist['name'])
+                        })
+
+                        console.log(`Track name: ${name}, artist: ${artistNames}`)
+
+                    })
+                    .catch(err => console.log('Error occurred'))
+
+        }
     } else {
         dialog.showErrorBox("Clipboard content not a Spotify link", "Go to Spotify and copy playlist or song link, then click 'Paste URL'")
     }
