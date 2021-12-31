01b085c8748f10efd876a21cdd4f591d200bd9e2
Noah <iebenmelu@gmail.com>
2021-12-30 20:46:44 -0800

    added functionality to `populateList()`

--- app/ui/main/lists.js
+++ app/ui/main/lists.js
@@ -1,22 +1,25 @@
 "use-strict";
 
 window.addEventListener("DOMContentLoaded", () => {
-  registerEventListeners();
   // retreive user downloads
   window.bridgeApis.invoke("get-list-data").then((data) => {
-    let loaders = document.querySelectorAll(".indeterminate-progress");
-
-    loaders.forEach((loader) => {
-      if (loader.id != "window-data-loader") loader.classList.add("gone");
-    });
     // display data to user
     if (data) {
       populateList(data);
+      // Now display the populated list items, becuase when they were created their visibility was set to gone.
+      document.getElementsByTagName("li").forEach((listElement) => listElement.classList.remove("gone"));
     } else {
       displayDecors();
     }
+
+    // display any indeterminate progress bar
+    document.querySelectorAll(".indeterminate-progress").forEach((loader) => {
+      if (loader.id != "window-data-loader") loader.classList.add("gone");
+    });
   });
 
+  // listen for event after populating the list
+  registerEventListeners();
   // actions related to file downloads
   window.bridgeApis.on("download-progress-update", (_event, args) => {
     let [listPos, progress] = args;
@@ -35,15 +38,118 @@ function populateList(data) {
   listDownloads.append(createListItemDownloads(data[0]));
   listDownloading.append(createListItemDownloading(data[1]));
 
+  // populate the 'downloaded' - list with item fetched from database
   function createListItemDownloads(item) {
-    return null;
+    const listSize = item["length"] || 1;
+    const uLElement = document.createElement("ul");
+    uLElement.classList.add("list-group");
+
+    // create the list items populating it with the fetched data from database
+    for (let i = 0; i < listSize; i++) {
+      const listElement = document.createElement("li");
+      listElement.classList.add("list-group-item", "gone"); // create but don't display yet
+      // create the thumbnail element
+      const thumbnailElement = document.createElement("img");
+      thumbnailElement.classList.add("media-object", "pull-left");
+      thumbnailElement.setAttribute("src", state["thumbnail"]);
+      // finally append those element node to the list parent node
+      listElement.append(thumbnailElement);
+      listElement.append(createMediaBody());
+      // append list item to list
+      uLElement.append(listElement);
+    }
+
+    // creates a media body element
+    function createMediaBody(state) {
+      const mediaBody = document.createElement("div");
+      // create the track title
+      const trackTitleElement = document.createElement("strong");
+      trackTitleElement.innerText = state["trackTitle"];
+      // create the message element
+      const messageElement = document.createElement("p");
+      messageElement.innerText = state["message"];
+      messageElement.classList.add("message");
+      // create the icons for the media body
+      const opIconContainer1 = document.createElement("div");
+      const opIconContainer2 = document.createElement("div");
+      const opIcon1 = document.createElement("span");
+      const opIcon2 = document.createElement("span");
+      // ... classes
+      opIconContainer1.classList.add("op-icon", "not-draggable", "pull-right");
+      opIconContainer1.classList.add("op-icon", "not-draggable", "pull-right");
+      // ..
+      opIcon1.classList.add("icon", "icon-folder", "icon-x2");
+      opIcon2.classList.add("icon", "icon-trash", "icon-x2");
+      opIconContainer1.append(opIcon1);
+      opIconContainer2.append(opIcon2);
+
+      mediaBody.append(trackTitleElement);
+      mediaBody.append(messageElement);
+      mediaBody.append(opIconContainer1);
+      mediaBody.append(opIconContainer2);
+
+      return mediaBody;
+    }
+
+    return uLElement;
   }
 
+  // populate the 'downloading' - list with item fetched from database
   function createListItemDownloading(item) {
-    return null;
+    const listSize = item["length"] || 1;
+    const uLElement = document.createElement("ul");
+    uLElement.classList.add("list-group");
+
+    // create the list items populating it with the fetched data from database
+    for (let i = 0; i < listSize; i++) {
+      const listElement = document.createElement("li");
+      listElement.classList.add("list-group-item", "gone"); // create but don't display yet
+      // create the thumbnail element
+      const thumbnailElement = document.createElement("img");
+      thumbnailElement.classList.add("media-object", "pull-left");
+      thumbnailElement.setAttribute("src", state["thumbnail"]);
+      // finally append those element node to the list parent node
+      listElement.append(thumbnailElement);
+      listElement.append(createMediaBody());
+      // append list item to list
+      uLElement.append(listElement);
+    }
+
+    // creates a media body element
+    function createMediaBody(state) {
+      const mediaBody = document.createElement("div");
+      // create the track title
+      const trackTitleElement = document.createElement("strong");
+      trackTitleElement.innerText = state["trackTitle"];
+      // create the message element
+      const messageElement = document.createElement("p");
+      messageElement.innerText = state["message"];
+      messageElement.classList.add("message");
+      // create the icons for the media body
+      const opIconContainer = document.createElement("div");
+      const opIcon = document.createElement("span");
+      // ... classes
+      opIcon.classList.add("icon", "icon-pause", "icon-x2");
+      opIconContainer.classList.add("op-icon", "not-draggable", "pull-right");
+      opIconContainer.append(opIcon);
+      // create the progress bar element
+      const downloadProgressElement = document.createElement("div");
+      downloadProgressElement.classList.add("horizontal-progress");
+      downloadProgressElement.id = `download-progress${state["listPos"]}`;
+      // finally append the created element nodes as children to the parent media body node
+      mediaBody.append(trackTitleElement);
+      mediaBody.append(messageElement);
+      mediaBody.append(opIconContainer);
+      mediaBody.append(downloadProgressElement);
+
+      return mediaBody;
+    }
+
+    return uLElement;
   }
 }
 
+// Register all event listeners on the list section UI in here
 function registerEventListeners() {
   let navItems = document.querySelectorAll(".nav-group-item");
 
