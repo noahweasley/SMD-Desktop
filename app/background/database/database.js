c31fad6b2e13e07e55cc3051788119564ea9092a
Noah <iebenmelu@gmail.com>
2022-01-03 05:22:22 -0800

    added some dependencies

--- app/background/database/database.js
+++ app/background/database/database.js
@@ -1,18 +1,18 @@
-// require("sqlite3");
-// const knex = require("knex");
-// const path = require("path");
-// const { app } = require("electron");
+require("sqlite3");
+const knex = require("knex");
+const path = require("path");
+const { app } = require("electron");
 
-// /**
-//  * 
-//  */
-// module.exports.database = knex({
-//   client: "sqlite",
-//   useNullAsDefault: true,
-//   connection: {
-//     filename: path.join(app.getPath("userData"), "User", "Database", "UserDB.sqlite"),
-//   },
-// });
+/**
+ * 
+ */
+module.exports.database = knex({
+  client: "sqlite",
+  useNullAsDefault: true,
+  connection: {
+    filename: path.join(app.getPath("userData"), "User", "Database", "UserDB.sqlite"),
+  },
+});
 
 /**
  * @returns the list data as stored in the application's database
