90983a3dfab587a45720ae0b5beafb3af73a7916
Noah <iebenmelu@gmail.com>
2022-01-03 17:42:03 -0800

    added OS Identifier

--- docs/action.js
+++ docs/action.js
@@ -7,3 +7,40 @@ function toggleNav() {
     nav.className = "topnav";
   }
 }
+
+function getOS() {
+  let userAgent = window.navigator.userAgent,
+    platform = window.navigator.platform,
+    macosPlatforms = ["Macintosh", "MacIntel", "MacPPC", "Mac68K"],
+    windowsPlatforms = ["Win32", "Win64", "Windows", "WinCE"],
+    os = null;
+
+  if (macosPlatforms.indexOf(platform) !== -1) {
+    os = "Mac OS";
+  } else if (windowsPlatforms.indexOf(platform) !== -1) {
+    os = "Windows";
+  } else if (!os && /Linux/.test(platform)) {
+    os = "Linux";
+  }
+
+  return os;
+}
+
+const downloadButton = document.querySelector(".download");
+const osSpan = downloadButton.lastChild;
+const osIcon = downloadButton.firstChild;
+
+let os = getOS();
+
+if (os) {
+  osSpan.innerText = os;
+  if (os == "Windows") {
+    osIcon.className = "fa fa-windows";
+  } else if (os == "Mac Os") {
+    osIcon.className = "fa fa-apple";
+  } else {
+    osIcon.className = "fa fa-linux";
+  }
+} else {
+  osSpan.innerText = "Not supported on your device";
+}
