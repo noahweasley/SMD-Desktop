0c399541f2fb2dc62b5045cdb293129500962895
Noah <iebenmelu@gmail.com>
2022-01-04 20:56:26 -0800

    adding terms & conditions and privacy policy page

--- docs/action.js
+++ docs/action.js
@@ -10,16 +10,6 @@ window.onscroll = function () {
   }
 };
 
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function toggleNav() {
-  const nav = document.getElementById("top-nav");
-  if (nav.className === "topnav") {
-    nav.className += " responsive";
-  } else {
-    nav.className = "topnav";
-  }
-}
-
 function getOS() {
   let userAgent = window.navigator.userAgent,
     platform = window.navigator.platform,
