4e368d87691214c97009bc1967037c828d670c9c
Noah <iebenmelu@gmail.com>
2022-02-23 21:01:24 -0800

    updated product website; adding proper download page

--- docs/action.js
+++ docs/action.js
@@ -26,26 +26,6 @@ function getOS() {
   return os;
 }
 
-const downloadButton = document.querySelector(".download");
-const osSpan = document.querySelector(".os");
-const osIcon = downloadButton.firstChild;
-
-let os = getOS();
-
-if (os) {
-  osSpan.innerText = os;
-  if (os == "Windows") {
-    osIcon.className = "fa fa-windows";
-  } else if (os == "Mac Os") {
-    osIcon.className = "fa fa-apple";
-  } else {
-    osIcon.className = "fa fa-linux";
-  }
-} else {
-  downloadButton.innerText = "Not supported on your device";
-  downloadButton.setAttribute("disabled", "true");
-}
-
 // date
 const date = document.querySelector(".date");
 date.innerText = new Date(Date.now()).getFullYear();
