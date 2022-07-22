701c752a56fe249aede7289db8b66815594ad606
Noah <iebenmelu@gmail.com>
2022-07-22 12:14:04 -0700

    refactor code and add dynamic title to main window

--- app/ui/main/setup.js
+++ app/ui/main/setup.js
@@ -7,20 +7,23 @@ function dataReveal() {
   const titileBar = document.querySelector(".toolbar-header");
   titileBar.classList.remove("gone");
 
-  window.bridgeApis
-    .invoke("get-multiple-states", ["spotify-secrets-received", "yt-api-key-received"])
-    .then((value) => {
-      loader.classList.add("gone");
-      if (value[0] === "true") {
-        windowContent.classList.remove("gone");
-        modal.style.setProperty("display", "none");
-      } else {
-        modal.style.setProperty("display", "flex");
-      }
-    });
+  window.bridgeApis.invoke("get-multiple-states", ["spotify-secrets-received", "yt-api-key-received"]).then((value) => {
+    loader.classList.add("gone");
+    if (value[0] === "true") {
+      windowContent.classList.remove("gone");
+      modal.style.setProperty("display", "none");
+    } else {
+      modal.style.setProperty("display", "flex");
+    }
+  });
 }
 
 window.addEventListener("DOMContentLoaded", () => {
   // 3 seconds time delay before displaying contents to user ...
   setTimeout(dataReveal, 3000);
+
+  window.bridgeApis.invoke("app-details").then((content) => {
+    const title = document.querySelector(".title");
+    title.innerText = content[0];
+  });
 });
