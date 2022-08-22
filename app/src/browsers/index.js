48528a52cf35ffc940f61f1a87e3c98f15467739
Noah <iebenmelu@gmail.com>
2022-08-22 14:07:34 -0700

    remove single-search and merged logic

--- app/src/browsers/index.js
+++ app/src/browsers/index.js
@@ -3,9 +3,6 @@ module.exports = (settings) => {
     aboutWindow: require("./about"),
     downloadWindow: require("./download"),
     mainWindow: require("./main")(settings),
-    multiSearchWindow: require("./multi-search"),
-    singleSearchWindow: require("./single-search")
+    searchWindow: require("./search")
   };
-  
 };
-
