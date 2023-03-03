a5854b10736db65d938904db6c6594f90280ecfc
noahweasley <iebenmelu@gmail.com>
2023-03-03 09:22:06 +0100

    add iconUrl and console colors using chalk

--- package/electron-windows-installer.js
+++ package/electron-windows-installer.js
@@ -1,10 +1,18 @@
 const electronInstaller = require("electron-winstaller");
-const appVersion = require("../package.json").version;
+const chalk = require("chalk");
+const { version: appVersion, productName } = require("../package.json");
+
+const error0 = chalk.bold.red;
+const success0 = chalk.green;
+const info0 = chalk.blue;
 
 const appExecutableName = "S.M.D_Desktop";
+const githubIconUrl =
+  "https://raw.githubusercontent.com/noahweasley/SMD-Desktop/master/app/resources/build/icons/win/icon.ico";
 
 const settings64 = {
   name: appExecutableName,
+  iconUrl: githubIconUrl,
   loadingGif: "./app/resources/build/tools/smd_installer.gif",
   setupIcon: "./app/resources/build/icons/win/icon.ico",
   appDirectory: "./build/dist/win/SMD-Desktop-win32-x64",
@@ -15,6 +23,7 @@ const settings64 = {
 
 const settings32 = {
   name: appExecutableName,
+  iconUrl: githubIconUrl,
   loadingGif: "./app/resources/build/tools/smd_installer.gif",
   setupIcon: "./app/resources/build/icons/win/icon.ico",
   appDirectory: "./build/dist/win/SMD-Desktop-win32-ia32",
@@ -25,10 +34,16 @@ const settings32 = {
 
 (async function () {
   try {
+    console.log(info0("Generating window installers, please wait..."));
+
     await electronInstaller.createWindowsInstaller(settings32);
+    console.log(info0("Generated x32 bit window installers successfully"));
+
     await electronInstaller.createWindowsInstaller(settings64);
-    console.log("The installers of your application were successfully created !");
+    console.log(info0("Generated x64 bit window installers successfully"));
+
+    console.log(success0(`The installers for ${productName} were successfully created !`));
   } catch (error) {
-    console.log(`Installer generation of ${appExecutableName} failed: ${error.message}`);
+    console.log(error0(`Installer generation of ${productName} failed: ${error.message}`));
   }
 })();
