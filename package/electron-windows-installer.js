10e08648727b932825e15f17e40d5ed4ceb8812f
noahweasley <iebenmelu@gmail.com>
2023-03-03 07:53:16 +0100

    change installer loading gif

--- package/electron-windows-installer.js
+++ package/electron-windows-installer.js
@@ -1,39 +1,34 @@
 const electronInstaller = require("electron-winstaller");
 const appVersion = require("../package.json").version;
 
-const appDisplayName = "S.M.D-Desktop";
-const appExecutableName = "smddesktop";
+const appExecutableName = "S.M.D_Desktop";
 
 const settings64 = {
   name: appExecutableName,
+  loadingGif: "./app/resources/build/tools/smd_installer.gif",
   setupIcon: "./app/resources/build/icons/win/icon.ico",
   appDirectory: "./build/dist/win/SMD-Desktop-win32-x64",
-  setupExe: `${appDisplayName}-setup.exe`,
+  setupExe: `${appExecutableName}-setup.exe`,
   outputDirectory: "./build/installers/win/x64",
   version: appVersion // use the version from package.json
 };
 
 const settings32 = {
   name: appExecutableName,
+  loadingGif: "./app/resources/build/tools/smd_installer.gif",
   setupIcon: "./app/resources/build/icons/win/icon.ico",
   appDirectory: "./build/dist/win/SMD-Desktop-win32-ia32",
-  setupExe: `${appDisplayName}-setup.exe`,
+  setupExe: `${appExecutableName}-setup.exe`,
   outputDirectory: "./build/installers/win/x32",
   version: appVersion // use the version from package.json
 };
 
-const createWindowsInstaller = Promise.all([
-  electronInstaller.createWindowsInstaller(settings32),
-  electronInstaller.createWindowsInstaller(settings64)
-]);
-
-createWindowsInstaller
-  .then(
-    () => {
-      console.log("The installers of your application were successfully created !");
-    },
-    (error) => {
-      console.log(`Installer generation of ${appDisplayName} failed: ${error.message}`);
-    }
-  )
-  .catch((error) => console.log(`Installer generation of ${appDisplayName} failed: ${error.message}`));
+(async function () {
+  try {
+    await electronInstaller.createWindowsInstaller(settings32);
+    await electronInstaller.createWindowsInstaller(settings64);
+    console.log("The installers of your application were successfully created !");
+  } catch (error) {
+    console.log(`Installer generation of ${appExecutableName} failed: ${error.message}`);
+  }
+})();
