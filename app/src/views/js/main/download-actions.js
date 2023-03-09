c51e2d959102b2f692f04e7c7d535b8c6d8559d0
noahweasley <iebenmelu@gmail.com>
2023-03-09 18:06:45 +0100

    finish implementing delete all downloaded task

--- app/src/views/js/main/download-actions.js
+++ app/src/views/js/main/download-actions.js
@@ -2,10 +2,27 @@
 
 window.addEventListener("DOMContentLoaded", () => {
   const deleteAllButton = document.querySelector(".downloads-operation .delete");
+  const downloadedList = document.querySelector(".list-group__downloaded");
+  const downloadingList = document.querySelector(".list-group__downloading");
 
   deleteAllButton.addEventListener("click", () => {
     const currentActiveTab = getCurrentActiveTab();
-    window.bridgeApis.invoke("delete-all", currentActiveTab);
+    window.bridgeApis.invoke("delete-all", currentActiveTab).then((isSuccessful) => {
+      if (isSuccessful) {
+        if (currentActiveTab == ".tab-content__downloaded") {
+          downloadedList.innerHTML = null;
+          displayInfoPlaceholderById("info_decor__downloaded", true);
+        } else {
+          downloadingList.innerHTML = null;
+          displayInfoPlaceholderById("info_decor__downloading", true);
+        }
+      } else {
+        window.bridgeApis.send("show-error-unknown-dialog", {
+          title: "An unknown error occurred",
+          message: "We were unable to complete some operations"
+        });
+      }
+    });
   });
 
   function getCurrentActiveTab() {
@@ -17,3 +34,13 @@ window.addEventListener("DOMContentLoaded", () => {
     return isDownloadingActive ? downloadingContent : downloadedContent;
   }
 });
+
+function displayInfoPlaceholderById(placeholderId, shouldShow) {
+  const infoPlaceholder = document.getElementById(placeholderId);
+  if (shouldShow) {
+    infoPlaceholder.style.setProperty("display", "flex");
+  } else {
+    infoPlaceholder.classList.remove("gone");
+    infoPlaceholder.style.setProperty("display", "none");
+  }
+}
