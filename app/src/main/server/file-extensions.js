a400b8292933c0572388d65e612812bf1f9bbb7b
noahweasley <iebenmelu@gmail.com>
2023-04-11 20:56:04 +0100

    Assign exported object to `module.exports` directly

--- app/src/main/server/file-extensions.js
+++ app/src/main/server/file-extensions.js
@@ -1,9 +1,7 @@
 "use-strict";
 
 // more file extensions to be added in the future
-const FILE_EXTENSIONS = Object.freeze({
+module.exports = Object.freeze({
   M4A: ".m4a",
   EXE: ".exe"
 });
-
-module.exports = FILE_EXTENSIONS;
