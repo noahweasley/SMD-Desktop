1017543fbb8edf752c6b3e92b9299b2dc09b6696
noahweasley <iebenmelu@gmail.com>
2023-04-19 18:16:17 +0100

    Update installer generation build script

--- package/electron-windows-installer.js
+++ package/electron-windows-installer.js
@@ -8,33 +8,34 @@ const info0 = chalk.blue;
 const info1 = chalk.yellow;
 
 const sep = "-";
-const appDisplayName = `S.M.D${sep}Desktop`;
-const appExecutableNameX32 = `${appDisplayName}${sep}x32${sep}v${appVersion}`;
-const appExecutableNameX64 = `${appDisplayName}${sep}x64${sep}v${appVersion}`;
+const appExecutableNameX32 = `${productName}${sep}x32${sep}v${appVersion}${sep}setup.exe`;
+const appExecutableNameX64 = `${productName}${sep}x64${sep}v${appVersion}${sep}setup.exe`;
 
 const githubIconUrl =
   "https://raw.githubusercontent.com/noahweasley/SMD-Desktop/master/app/resources/build/icons/win/icon.ico";
 
 const settings64 = {
-  name: appDisplayName,
+  noMsi: true,
+  name: productName,
   iconUrl: githubIconUrl,
   loadingGif: "./app/resources/build/tools/smd_installer.gif",
   setupIcon: "./app/resources/build/icons/win/icon.ico",
-  appDirectory: "./build/dist/win/SMD-Desktop-win32-x64",
-  setupExe: `${appExecutableNameX64}-setup.exe`,
+  appDirectory: `./build/dist/win/${productName}-win32-x64`,
+  setupExe: appExecutableNameX64,
   outputDirectory: "./build/installers/win/x64",
-  version: appVersion // use the version from package.json
+  version: appVersion
 };
 
 const settings32 = {
-  name: appDisplayName,
+  noMsi: true,
+  name: productName,
   iconUrl: githubIconUrl,
   loadingGif: "./app/resources/build/tools/smd_installer.gif",
   setupIcon: "./app/resources/build/icons/win/icon.ico",
-  appDirectory: "./build/dist/win/SMD-Desktop-win32-ia32",
-  setupExe: `${appExecutableNameX32}-setup.exe`,
+  appDirectory: `./build/dist/win/${productName}-win32-ia32`,
+  setupExe: appExecutableNameX32,
   outputDirectory: "./build/installers/win/x32",
-  version: appVersion // use the version from package.json
+  version: appVersion
 };
 
 (async function () {
@@ -51,6 +52,6 @@ const settings32 = {
 
     console.log(success0(`The installers for ${productName} were successfully created!!`));
   } catch (error) {
-    console.log(error0(`Installer generation of ${productName} failed \n Message \n ${error.message}`));
+    console.log(error0(`Installer generation of ${productName} failed\n${error.message}`));
   }
 })();
