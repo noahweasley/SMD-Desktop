dc0fa8ff4d2933360488af2e7544a2241b451355
noahweasley <iebenmelu@gmail.com>
2023-04-19 16:36:29 +0100

    Add build clean script

---
+++ scripts/del-build.js
@@ -0,0 +1,16 @@
+const { rm } = require("fs");
+const { join } = require("path");
+const chalk = require("chalk");
+
+const info0 = chalk.blue;
+const error0 = chalk.bold.red;
+const success0 = chalk.green;
+
+console.log(info0("Deleting build folder"));
+rm(join(__dirname, "../build"), { recursive: true, force: true }, (error) => {
+  if (error) {
+    console.error(error0(error));
+  } else {
+    console.log(success0("Build folder deleted"));
+  }
+});
