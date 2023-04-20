cae5356c688da44b6568019895d5ee817014b60e
noahweasley <iebenmelu@gmail.com>
2023-04-20 18:36:03 +0100

    Update script

--- scripts/tasks/clean-build.js
+++ scripts/tasks/clean-build.js
@@ -6,12 +6,12 @@ const info0 = chalk.blue;
 const error0 = chalk.bold.red;
 const success0 = chalk.green;
 
-console.log(info0("Deleting build folder..."));
+console.log(info0("Cleaning build..."));
 
 rm(join(__dirname, "../build"), { recursive: true, force: true }, (error) => {
   if (error) {
     console.error(error0(error));
   } else {
-    console.log(success0("Build folder deleted"));
+    console.log(success0("Build cleaned"));
   }
 });
