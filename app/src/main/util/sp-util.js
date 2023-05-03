0019c5270d5812aa47dd82c00c8a3dc3d1a559b8
noahweasley <iebenmelu@gmail.com>
2023-05-03 06:50:55 +0100

    Improve code quality, removing confusing code

--- app/src/main/util/sp-util.js
+++ app/src/main/util/sp-util.js
@@ -1,31 +1,36 @@
 const { clipboard } = require("electron");
 
-//! don't touch SpotifyURLType, I tried to remove it once and there was a bug
-//* I also don't even know what is depending on it, lol
+function __exports() {
+  /**
+   * The list of supported links
+   */
+  const SpotifyURLType = Object.freeze({
+    TRACK: "track",
+    PLAYLIST: "playlist",
+    ALBUM: "album",
+    ARTIST: "artist",
+    UNKNOWN: "Unknown"
+  });
 
-// eslint-disable-next-line no-undef
-module.exports.SpotifyURLType = SpotifyURLType = Object.freeze({
-  TRACK: "track",
-  PLAYLIST: "playlist",
-  ALBUM: "album",
-  ARTIST: "artist",
-  UNKNOWN: "Unknown"
-});
+  /**
+   * @param {string} url the spotify url to be checked
+   * @returns the type of url it is
+   */
+  function getSpotifyURLType(url) {
+    const clipboardContent = url || clipboard.readText();
 
-/**
- * @param {string} url the spotify url to be checked
- * @returns the type of url it is
- */
-module.exports.getSpotifyURLType = function (url) {
-  const clipboardContent = url || clipboard.readText();
-
-  if (clipboardContent.includes("https://open.spotify.com")) {
-    if (clipboardContent.search(this.SpotifyURLType.TRACK) !== -1) return this.SpotifyURLType.TRACK;
-    else if (clipboardContent.search(this.SpotifyURLType.PLAYLIST) !== -1) return this.SpotifyURLType.PLAYLIST;
-    else if (clipboardContent.search(this.SpotifyURLType.ALBUM) !== -1) return this.SpotifyURLType.ALBUM;
-    else if (clipboardContent.search(this.SpotifyURLType.ARTIST) !== -1) return this.SpotifyURLType.ARTIST;
-    else return this.SpotifyURLType.UNKNOWN;
-  } else {
-    throw new Error("Uh ohh !! That wasn't a spotify url");
+    if (clipboardContent.includes("https://open.spotify.com")) {
+      if (clipboardContent.search(SpotifyURLType.TRACK) !== -1) return SpotifyURLType.TRACK;
+      else if (clipboardContent.search(SpotifyURLType.PLAYLIST) !== -1) return SpotifyURLType.PLAYLIST;
+      else if (clipboardContent.search(SpotifyURLType.ALBUM) !== -1) return SpotifyURLType.ALBUM;
+      else if (clipboardContent.search(SpotifyURLType.ARTIST) !== -1) return SpotifyURLType.ARTIST;
+      else return SpotifyURLType.UNKNOWN;
+    } else {
+      throw new Error("Uh ohh !! That wasn't a spotify url");
+    }
   }
-};
+
+  return { SpotifyURLType, getSpotifyURLType };
+}
+
+module.exports = __exports();
